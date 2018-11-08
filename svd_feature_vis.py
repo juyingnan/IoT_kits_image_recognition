ad5e85f4b240b36de1533fd9847ddc883137e8c8
Yingnan Ju <bunny_gg@hotmail.com>
2018-11-08 00:49:35 -0500

    better svd

--- svd_feature_vis.py
+++ svd_feature_vis.py
@@ -2,11 +2,11 @@ import matplotlib.pyplot as plt
 from scipy import io
 import numpy as np
 
-mat_path = r'D:\Projects\IoT_recognition\20181028\vis\ratio_hue.mat'
+mat_path = r'D:\Projects\IoT_recognition\20181028\vis\ratio_color.mat'
 digits = io.loadmat(mat_path)
 X, y = digits.get('feature_matrix'), digits.get('label')[0]
-X=X[::10]
-y=y[::10]
+X = X[::5]
+y = y[::5]
 n_samples, n_features = X.shape
 
 eigenvalues, eigenvectors = np.linalg.eig(np.cov(X))
@@ -16,45 +16,104 @@ U, s, Vh = np.linalg.svd(X, full_matrices=False)
 s[2:] = 0
 
 fig = plt.figure()
-ax = fig.add_subplot(221)
-ax.bar(np.arange(len(X[0])), X[0])
+ax = fig.add_subplot(321)
+# ax.bar(np.arange(len(X[0])), X[0])
+ax.imshow(X.transpose())
+if "raw" not in mat_path:
+    ax.set_aspect(100)
 ax.set_title('original_mat')
 
-ax = fig.add_subplot(222)
+ax = fig.add_subplot(322)
 ax.bar(np.arange(len(eigenvalues)), eigenvalues)
 ax.set_title('eigenvalues_feature')
 
-ax = fig.add_subplot(223)
-ax.set_ylim(-1, 1)
-ax.set_xlim(-1, 1)
-ax.grid(True, which='both')
+small_edge_index = 0.2
+ax = fig.add_subplot(323)
+ax.grid(True, which='both', color='#CFCFCF')
 ax.axhline(y=0, color='k')
 ax.axvline(x=0, color='k')
 ev1 = eigenvectors[0].real
 ev2 = eigenvectors[1].real
-for i in range(X.transpose().shape[0]):
-    xx = X.transpose()[i].dot(ev1)
-    yy = X.transpose()[i].dot(ev2)
-    ax.text(xx, yy, str(i), color=plt.cm.tab20(i), fontdict={'size': 8})
-ax.set_title('features')
-bottom, top = plt.ylim()
+xx = X.transpose().dot(ev1)
+yy = X.transpose().dot(ev2)
+small_edge = (max(yy) - min(yy)) * small_edge_index
+ax.set_ylim(min(yy) - small_edge if min(yy) <= -small_edge else -small_edge,
+            max(yy) + small_edge if max(yy) >= small_edge else small_edge)
+small_edge = (max(xx) - min(xx)) * small_edge_index
+ax.set_xlim(min(xx) - small_edge if min(xx) <= -small_edge else -small_edge,
+            max(xx) + small_edge if max(xx) >= small_edge else small_edge)
+for i in range(xx.shape[0]):
+    if "raw" in mat_path:
+        ax.text(xx[i], yy[i], '.', fontdict={'size': 10})
+    else:
+        ax.text(xx[i], yy[i], str(i), fontdict={'size': 8})
+ax.set_title('features_projection')
+
+ax = fig.add_subplot(324)
+ax.grid(True, which='both', color='#CFCFCF')
+ax.axhline(y=0, color='k')
+ax.axvline(x=0, color='k')
+x_mean = X.transpose().mean(axis=1, keepdims=True)
+xx = (X.transpose() - x_mean).dot(ev1 - ev1.mean())
+yy = (X.transpose() - x_mean).dot(ev2 - ev2.mean())
+r_x, r_y = [], []
+for i in range(xx.shape[0]):
+    r_x.append(xx[i] / np.linalg.norm(X.transpose()[i] - x_mean[i]) / np.linalg.norm(ev1 - ev1.mean()))
+    r_y.append(yy[i] / np.linalg.norm(X.transpose()[i] - x_mean[i]) / np.linalg.norm(ev2 - ev2.mean()))
+edge_max = max([abs(max(r_x)), abs(min(r_x)), abs(max(r_y)), abs(min(r_y))]) * (1 + small_edge_index)
+ax.set_ylim(-edge_max, edge_max)
+ax.set_xlim(-edge_max, edge_max)
+for i in range(xx.shape[0]):
+    if "raw" in mat_path:
+        ax.text(r_x[i], r_y[i], '.', fontdict={'size': 10})
+    else:
+        ax.text(r_x[i], r_y[i], str(i), fontdict={'size': 8})
+ax.set_title('features_correlation')
 
 eigenvalues, eigenvectors = np.linalg.eig(np.cov(X.transpose()))
 U, s, Vh = np.linalg.svd(X.transpose(), full_matrices=False)
 s[2:] = 0
 
-ax = fig.add_subplot(224)
-ax.set_ylim(-1, 1)
-ax.set_xlim(-1, 1)
-ax.grid(True, which='both')
+ax = fig.add_subplot(325)
+ax.grid(True, which='both', color='#CFCFCF')
 ax.axhline(y=0, color='k')
 ax.axvline(x=0, color='k')
 ev1 = eigenvectors[0].real
 ev2 = eigenvectors[1].real
+xx = X.dot(ev1)
+yy = X.dot(ev2)
+small_edge = (max(yy) - min(yy)) * small_edge_index
+ax.set_ylim(min(yy) - small_edge if min(yy) <= -small_edge else -small_edge,
+            max(yy) + small_edge if max(yy) >= small_edge else small_edge)
+small_edge = (max(xx) - min(xx)) * small_edge_index
+ax.set_xlim(min(xx) - small_edge if min(xx) <= -small_edge else -small_edge,
+            max(xx) + small_edge if max(xx) >= small_edge else small_edge)
 for i in range(X.shape[0]):
-    xx = X[i].dot(ev1)
-    yy = X[i].dot(ev2)
-    ax.text(xx, yy, str(y[i]), color=plt.cm.tab20(int(y[i])), fontdict={'size': 8})
-ax.set_title('samples')
+    ax.text(xx[i], yy[i], str(y[i] + 1), color=plt.cm.tab20(int(y[i])), fontdict={'size': 8})
+ax.set_title('samples_projection')
+
+eigenvalues, eigenvectors = np.linalg.eig(np.cov(X.transpose()))
+U, s, Vh = np.linalg.svd(X.transpose(), full_matrices=False)
+s[2:] = 0
+
+ax = fig.add_subplot(326)
+ax.grid(True, which='both', color='#CFCFCF')
+ax.axhline(y=0, color='k')
+ax.axvline(x=0, color='k')
+ev_mean1 = ev1.mean()
+ev_mean2 = ev2.mean()
+x_mean = X.mean(axis=1, keepdims=True)
+xx = (X - x_mean).dot(ev1 - ev1.mean())
+yy = (X - x_mean).dot(ev2 - ev2.mean())
+r_x, r_y = [], []
+for i in range(xx.shape[0]):
+    r_x.append(xx[i] / np.linalg.norm(X[i] - x_mean[i]) / np.linalg.norm(ev1 - ev1.mean()))
+    r_y.append(yy[i] / np.linalg.norm(X[i] - x_mean[i]) / np.linalg.norm(ev2 - ev2.mean()))
+edge_max = max([abs(max(r_x)), abs(min(r_x)), abs(max(r_y)), abs(min(r_y))]) * (1 + small_edge_index)
+ax.set_ylim(-edge_max, edge_max)
+ax.set_xlim(-edge_max, edge_max)
+for i in range(X.shape[0]):
+    ax.text(r_x[i], r_y[i], str(y[i] + 1), color=plt.cm.tab20(int(y[i])), fontdict={'size': 8})
+ax.set_title('samples_correlation')
 
 plt.show()
